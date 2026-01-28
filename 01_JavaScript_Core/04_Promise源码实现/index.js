3c6d9713cdf1e9aab301ee451f3b47eba92ab762
浅蓝 <5266154+yt1997@user.noreply.gitee.com>
2026-01-28 18:36:01 +0800

    feat：修改实现，待完善

--- 01_JavaScript_Core/04_Promise源码实现/index.js
+++ 01_JavaScript_Core/04_Promise源码实现/index.js
@@ -1,65 +1,47 @@
-class MyPromise {
-  constructor(fn) {
-    this.status = "PENDING";
-    this.fulfilledFns = new Set();
-    this.rejectedFns = new Set();
-    const resolve = (value) => {
-      this.status = "FULFILLED";
-      this.value = value;
-      if (this.fulfilledFns.size > 0) {
-        for (let fn of this.fulfilledFns) {
-          queueMicrotask(() => fn(this.value));
-        }
-      }
+class PromiseIml {
+  static PENDING = "PENDING";
+  static FULFILLED = "FULFILLED";
+  static REJECTED = "REJECTED";
+  constructor(executeFn) {
+    // 状态 - 不可逆
+    this.status = PromiseIml.PENDING;
+    // resolve值
+    this.value = null;
+    // reject值
+    this.reason = null;
+    // 存储resolve回调函数，在状态改变为FULFILLED调用
+    this.fulfilledLists = [];
+    // 存储reject回调函数，在状态改变为REJECTED调用
+    this.rejectedLists = [];
+    const resolve = (res) => {
+      if (this.status !== PromiseIml.PENDING) return;
+      this.status = PromiseIml.FULFILLED;
+      this.value = res;
+      // 调用then回调并且将结果传入回调参数
+      this.fulfilledLists.map((fn) => fn(this.value));
     };
-    const reject = (reason) => {
-      this.status = "REJECTED";
-      this.reason = reason;
-      if (this.rejectedFns.size > 0) {
-        for (let fn of this.rejectedFns) {
-          queueMicrotask(() => fn(this.value));
-        }
-      }
+    const reject = (res) => {
+      if (this.status !== PromiseIml.PENDING) return;
+      this.status = PromiseIml.REJECTED;
+      this.reason = res;
+      // 调用then回调并且将结果传入回调参数
+      this.fulfilledLists.map((fn) => fn(this.value));
     };
-    fn(resolve, reject);
-  }
-  then(onFulfilled) {
-    if (this.status === "PENDING") {
-      this.fulfilledFns.add(onFulfilled);
+    try {
+      executeFn(resolve, reject);
+    } catch (error) {
+      reject(error);
     }
-    return new MyPromise(onFulfilled);
   }
-  catch(onRejected) {
-    if (this.status === "PENDING") {
-      this.rejectedFns.add(onRejected);
-    }
-    return new MyPromise(onRejected);
+  then(resloveFn){
+    // if()
   }
 }
 
-new MyPromise((resolve, reject) => {
+new PromiseIml((resolve, reject) => {
   setTimeout(() => {
-    resolve("test111");
-  }, 2000);
+    resolve("test resolve");
+  }, 1200);
+}).then((res) => {
+  console.log('res', res)
 })
-  .then((res) => {
-    console.log("res: ", res);
-  })
-  .catch((err) => {
-    console.log("err: ", err);
-  })
-  .then((res) => {
-    console.log("res222: ", res);
-  });
-
-// new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     reject("11122");
-//   }, 1000);
-// })
-//   .then((res) => {
-//     console.log("res: ", res);
-//   })
-//   .catch((err) => {
-//     console.log("err: ", err);
-//   });
